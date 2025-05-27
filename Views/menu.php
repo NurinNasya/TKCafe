a3d8a5dcdd3af436fb7dcf87553aaa1c6f953b68
hazsuh <nurhaziqahsuhaimi04@gmail.com>
2025-05-27 09:32:16 +0800

    update

--- Views/menu.php
+++ Views/menu.php
@@ -10,6 +10,55 @@
 <div class="menu-items">
 
 
+     <!-- BEST SELLER -->
+
+   <div class="menu-item" data-category="best-seller">
+    <img src="/TKCafe/public/images/LAKSA PENANG.jpg" alt="Laksa Penang" class="menu-item-img" />
+    <div class="menu-item-info">
+      <div>
+        <h3> Laksa Penang </h3>
+        <p></p>
+        <div class="menu-item-price">RM8.00</div>
+      </div>
+      <div>
+        <button class="select-btn">Select</button>
+      </div>
+    </div>
+  </div>
+
+   <div class="menu-item" data-category="best-seller">
+    <img src="/TKCafe/public/images/TEA PING.png" alt="Laksa Asia" class="menu-item-img" />
+    <div class="menu-item-info">
+      <div>
+        <h3>Laksa Asia</h3>
+        <p></p>
+        <div class="menu-item-price">RM12.00</div>
+      </div>
+      <div>
+        <button class="select-btn">Select</button>
+      </div>
+    </div>
+  </div>
+
+  <div class="menu-item" data-category="best-seller">
+    <img src="/TKCafe/public/images/TEA PING.png" alt="Keropok Tumis" class="menu-item-img" />
+    <div class="menu-item-info">
+      <div>
+        <h3>Keropok Tumis</h3>
+        <p></p>
+        <div class="menu-item-price">RM5.00</div>
+      </div>
+      <div>
+        <button class="select-btn">Select</button>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+
 <!-- STANDARD MENU -->
 
   <div class="menu-item" data-category="standard">
@@ -270,7 +319,7 @@
   </div>
 
 
-  <!-- SET STANDARD -->
+  <!-- SET STANDARD COMBO-->
 
 
     <div class="menu-item" data-category="set-standard">
@@ -301,6 +350,8 @@
     </div>
   </div>
 
+
+
   <div class="menu-item" data-category="set-standard">
     <img src="/TKCafe/public/images/NASI MINYAK GULAI AYAM.jpg" alt="Nasi Minyak Gulai Ayam" class="menu-item-img" />
     <div class="menu-item-info">
@@ -315,7 +366,39 @@
     </div>
   </div>
 
- <div class="menu-item" data-category="set-signature">
+
+ <div class="menu-item" data-category="set-standard">
+    <img src="/TKCafe/public/images/NASI MINYAK AYAM MERAH.jpg" alt=">Nasi Minyak Ayam Merah" class="menu-item-img" />
+    <div class="menu-item-info">
+      <div>
+        <h3>Nasi Minyak Ayam Merah + Soft Drink</h3>
+        <p>Nasi Minyak Ayam Merah + Ayam Masak Merah + Acar</p>
+        <div class="menu-item-price">RM12.00</div>
+      </div>
+      <div>
+        <button class="select-btn" data-id="5">Select</button>
+      </div>
+    </div>
+  </div>
+
+<div class="menu-item" data-category="set-standard">
+    <img src="/TKCafe/public/images/NASI GORENG KAMPUNG.jpg" alt="Nasi Goreng Kampung" class="menu-item-img" />
+    <div class="menu-item-info">
+      <div>
+        <h3>Nasi Goreng Kampung  + Soft Drink</h3>
+        <p>Nasi Goreng Kampung</p>
+        <div class="menu-item-price">RM12.00</div>
+      </div>
+      <div>
+         <button class="select-btn" data-id="3">Select</button>
+      </div>
+    </div>
+  </div>
+
+
+
+
+ <div class="menu-item" data-category="set-standard">
     <img src="/TKCafe/public/images/NASI LEMAK SAMBAL UDANG.jpg" alt="Nasi Lemak Sambal Udang" class="menu-item-img" />
     <div class="menu-item-info">
       <div>
@@ -329,21 +412,23 @@
     </div>
   </div>
 
-  <div class="menu-item" data-category="set-signature">
-    <img src="/TKCafe/public/images/NASI MINYAK AYAM MERAH.jpg" alt="Nasi Minyak Ayam Merah" class="menu-item-img" />
+
+<div class="menu-item" data-category="set-signature">
+    <img src="/TKCafe/public/images/BIHUN GORENG SINGAPURA.png" alt="Bihun Goreng Singapura " class="menu-item-img" />
     <div class="menu-item-info">
       <div>
-        <h3>Nasi Minyak Ayam Merah + Soft Drink</h3>
-        <p>Nasi Minyak + Ayam Masak Merah + Acar</p>
-        <div class="menu-item-price">RM12.00</div>
+        <h3>Bihun Goreng Singapura  + Soft Drink </h3>
+        <p>Bihun Goreng Singapura</p>
+        <div class="menu-item-price">RM11.00</div>
       </div>
       <div>
-        <button class="select-btn">Select</button>
+         <button class="select-btn" data-id="3">Select</button>
       </div>
     </div>
   </div>
 
-  <div class="menu-item" data-category="set-signature">
+
+<div class="menu-item" data-category="set-standard">
     <img src="/TKCafe/public/images/NASI AYAM.png" alt="Nasi Ayam" class="menu-item-img" />
     <div class="menu-item-info">
       <div>
@@ -356,6 +441,165 @@
       </div>
     </div>
   </div>
+
+
+<div class="menu-item" data-category="set-standard">
+    <img src="/TKCafe/public/images/NASI TOMATO KUZI AYAM (1).jpg" alt="Nasi Tomato Kuzi Ayam" class="menu-item-img" />
+    <div class="menu-item-info">
+      <div>
+        <h3>Nasi Tomato Kuzi Ayam  + Soft Drink</h3>
+        <p>Nasi Tomato + Kuzi Ayam </p>
+        <div class="menu-item-price">RM13.50</div>
+      </div>
+      <div>
+         <button class="select-btn" data-id="3">Select</button>
+      </div>
+    </div>
+  </div>
+
+  <div class="menu-item" data-category="set-standard">
+    <img src="/TKCafe/public/images/NASI BERIANI AYAM GULAI BERIANI.png" alt="Nasi Beriani Ayam Gulai Beriani" class="menu-item-img" />
+    <div class="menu-item-info">
+      <div>
+        <h3>Nasi Beriani Ayam Gulai Beriani  + Soft Drink </h3>
+        <p>Nasi Beriani Ayam Gulai Beriani</p>
+        <div class="menu-item-price">RM14.00</div>
+      </div>
+      <div>
+         <button class="select-btn" data-id="3">Select</button>
+      </div>
+    </div>
+  </div>
+
+ <!-- SET SIGNATURE MENU-->
+
+
+    <div class="menu-item" data-category="set-signature">
+    <img src="/TKCafe/public/images/NASI LEMAK SAMBAL UDANG.jpg" alt="NASI DAGANG TERENGGANU + SOFT DRINKS" class="menu-item-img" />
+    <div class="menu-item-info">
+      <div>
+        <h3>Nasi Dagang Terengganu+ Soft Drink</h3>
+        <p>Nasi Dagang + Gulai Ikan + Acar</p>
+        <div class="menu-item-price">RM14.00</div>
+      </div>
+      <div>
+        <button class="select-btn">Select</button>
+      </div>
+    </div>
+  </div>
+
+  <div class="menu-item" data-category="set-signature">
+    <img src="/TKCafe/public/images/NASI LEMAK RENDANG AYAM.png" alt="Nasi Lemak Rendang Ayam" class="menu-item-img" />
+    <div class="menu-item-info">
+      <div>
+        <h3>Nasi Lemak Rendang Ayam + Soft Drink</h3>
+        <p>Nasi Lemak + Rendang Ayam</p>
+        <div class="menu-item-price">RM15.50</div>
+      </div>
+      <div>
+         <button class="select-btn" data-id="2">Select</button>
+      </div>
+    </div>
+  </div>
+
+
+
+  <div class="menu-item" data-category="set-signature">
+    <img src="/TKCafe/public/images/NASI MINYAK GULAI AYAM.jpg" alt="Nasi Minyak Gulai Ayam" class="menu-item-img" />
+    <div class="menu-item-info">
+      <div>
+        <h3>Nasi Minyak Gulai Ayam + Soft Drink</h3>
+        <p>Nasi Minyak + Gulai Ayam</p>
+        <div class="menu-item-price">RM14.00</div>
+      </div>
+      <div>
+         <button class="select-btn" data-id="3">Select</button>
+      </div>
+    </div>
+  </div>
+
+
+ <div class="menu-item" data-category="set-signature">
+    <img src="/TKCafe/public/images/NASI MINYAK AYAM MERAH.jpg" alt=">Nasi Minyak Ayam Merah" class="menu-item-img" />
+    <div class="menu-item-info">
+      <div>
+        <h3>Nasi Minyak Ayam Merah + Soft Drink</h3>
+        <p>Nasi Minyak Ayam Merah + Ayam Masak Merah + Acar</p>
+        <div class="menu-item-price">RM13.00</div>
+      </div>
+      <div>
+        <button class="select-btn" data-id="5">Select</button>
+      </div>
+    </div>
+  </div>
+
+
+
+
+ <div class="menu-item" data-category="set-signature">
+    <img src="/TKCafe/public/images/NASI LEMAK SAMBAL UDANG.jpg" alt="Nasi Lemak Sambal Udang" class="menu-item-img" />
+    <div class="menu-item-info">
+      <div>
+        <h3>Nasi Lemak Sambal Udang + Soft Drink</h3>
+        <p>Nasi Lemak + Sambal Udang</p>
+        <div class="menu-item-price">RM18.00</div>
+      </div>
+      <div>
+        <button class="select-btn">Select</button>
+      </div>
+    </div>
+  </div>
+
+
+
+<div class="menu-item" data-category="set-signature">
+    <img src="/TKCafe/public/images/NASI AYAM.png" alt="Nasi Ayam" class="menu-item-img" />
+    <div class="menu-item-info">
+      <div>
+        <h3>Nasi Ayam + Soft Drink</h3>
+        <p>Nasi + Ayam + Sup + Sambal</p>
+        <div class="menu-item-price">RM13.00</div>
+      </div>
+      <div>
+        <button class="select-btn">Select</button>
+      </div>
+    </div>
+  </div>
+
+
+<div class="menu-item" data-category="set-signature">
+    <img src="/TKCafe/public/images/NASI TOMATO KUZI AYAM (1).jpg" alt="Nasi Tomato Kuzi Ayam" class="menu-item-img" />
+    <div class="menu-item-info">
+      <div>
+        <h3>Nasi Tomato Kuzi Ayam  + Soft Drink</h3>
+        <p>Nasi Tomato + Kuzi Ayam </p>
+        <div class="menu-item-price">RM16.50</div>
+      </div>
+      <div>
+         <button class="select-btn" data-id="3">Select</button>
+      </div>
+    </div>
+  </div>
+
+  <div class="menu-item" data-category="set-signature">
+    <img src="/TKCafe/public/images/NASI BERIANI AYAM GULAI BERIANI.png" alt="Nasi Beriani Ayam Gulai Beriani" class="menu-item-img" />
+    <div class="menu-item-info">
+      <div>
+        <h3>Nasi Beriani Ayam Gulai Beriani  + Soft Drink </h3>
+        <p>Nasi Beriani Ayam Gulai Beriani</p>
+        <div class="menu-item-price">RM14.00</div>
+      </div>
+      <div>
+         <button class="select-btn" data-id="3">Select</button>
+      </div>
+    </div>
+  </div>
+
+
+
+
+ <!-- MASAKAN PANAS-->
+
 
    <div class="menu-item" data-category="masakan-ala">
     <img src="/TKCafe/public/images/NASI GORENG AYAM.png" alt="Nasi Goreng Ayam" class="menu-item-img" />
@@ -428,6 +672,7 @@
   </div>
 
 
+ <!-- SIDE DISH-->
 
      <div class="menu-item" data-category="masakan-side">
     <img src="/TKCafe/public/images/NASI LEMAK SAMBAL UDANG.jpg" alt="Nasi Lemak Sambal Udang" class="menu-item-img" />
@@ -470,6 +715,10 @@
       </div>
     </div>
   </div>
+
+
+  
+ <!-- ALA CART LOKCING-->
 
     <div class="menu-item" data-category="lokcing">
     <img src="/TKCafe/public/images/NASI AYAM.png" alt="Nasi Ayam" class="menu-item-img" />
@@ -515,7 +764,87 @@
     </div>
   </div>
 
-  <div class="menu-item" data-category="beverages">
+
+  <div class="menu-item" data-category="lokcing">
+    <img src="/TKCafe/public/images/SOSEJ CHEESE (1 PCS).png" alt="Sosej Cheese (1 PCS)" class="menu-item-img" />
+    <div class="menu-item-info">
+      <div>
+        <h3>Sosej Cheese (1 PCS)</h3>
+        <p></p>
+        <div class="menu-item-price">RM3.00</div>
+      </div>
+      <div>
+        <button class="select-btn">Select</button>
+      </div>
+    </div>
+  </div>
+
+  <div class="menu-item" data-category="lokcing">
+    <img src="/TKCafe/public/images/NUGGET TEMPURA (6 PCS).jpg" alt="Nugget Tempura (6PCS)" class="menu-item-img" />
+    <div class="menu-item-info">
+      <div>
+        <h3>Nugget Tempura (6 PCS)</h3>
+        <p></p>
+        <div class="menu-item-price">RM3.50</div>
+      </div>
+      <div>
+        <button class="select-btn">Select</button>
+      </div>
+    </div>
+  </div>
+
+
+
+ <!-- ALA CART WESTERN-->
+
+ <div class="menu-item" data-category="western">
+    <img src="/TKCafe/public/images/CHEESY WEDGES.png" alt="Cheesy Wedges" class="menu-item-img" />
+    <div class="menu-item-info">
+      <div>
+        <h3>Cheesy Wedges</h3>
+        <p></p>
+        <div class="menu-item-price">RM6.00</div>
+      </div>
+      <div>
+        <button class="select-btn">Select</button>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="menu-item" data-category="western">
+    <img src="/TKCafe/public/images/COLESLAW (REGULAR).png" alt="Coleslaw (Regular)" class="menu-item-img" />
+    <div class="menu-item-info">
+      <div>
+        <h3>Coleslaw (Regular)</h3>
+        <p></p>
+        <div class="menu-item-price">RM3.00</div>
+      </div>
+      <div>
+        <button class="select-btn">Select</button>
+      </div>
+    </div>
+  </div>
+
+  <div class="menu-item" data-category="western">
+    <img src="/TKCafe/public/images/FRENCH FRIES.png" alt="French Fries" class="menu-item-img" />
+    <div class="menu-item-info">
+      <div>
+        <h3>French Fries</h3>
+        <p></p>
+        <div class="menu-item-price">RM5.00</div>
+      </div>
+      <div>
+        <button class="select-btn">Select</button>
+      </div>
+    </div>
+  </div>
+
+
+
+ <!-- BEVERANGE-->
+
+  <div class="menu-item" data-category="air-balang">
     <img src="/TKCafe/public/images/ICE LEMON TEA.png" alt="Ice Lemon Tea" class="menu-item-img" />
     <div class="menu-item-info">
       <div>
@@ -529,7 +858,7 @@
     </div>
   </div>
 
-  <div class="menu-item" data-category="beverages">
+  <div class="menu-item" data-category="air-balang">
     <img src="/TKCafe/public/images/TEA PING.png" alt="Tea Ping" class="menu-item-img" />
     <div class="menu-item-info">
       <div>
@@ -543,6 +872,184 @@
     </div>
   </div>
 
+
+   <!-- CARBONATE DRINK-->
+
+ <div class="menu-item" data-category="soft-drinkss">
+    <img src="/TKCafe/public/images/ICE LEMON TEA.png" alt="Cola Cola" class="menu-item-img" />
+    <div class="menu-item-info">
+      <div>
+        <h3>Cola Cola</h3>
+        <p></p>
+        <div class="menu-item-price">RM3.00</div>
+      </div>
+      <div>
+        <button class="select-btn">Select</button>
+      </div>
+    </div>
+  </div>
+
+  <div class="menu-item" data-category="soft-drinks">
+    <img src="/TKCafe/public/images/TEA PING.png" alt="Sprite" class="menu-item-img" />
+    <div class="menu-item-info">
+      <div>
+        <h3>Sprite</h3>
+        <p></p>
+        <div class="menu-item-price">RM3.00</div>
+      </div>
+      <div>
+        <button class="select-btn">Select</button>
+      </div>
+    </div>
+  </div>
+
+
+   <div class="menu-item" data-category="soft-drinks">
+    <img src="/TKCafe/public/images/ICE LEMON TEA.png" alt="F&N Orange" class="menu-item-img" />
+    <div class="menu-item-info">
+      <div>
+        <h3> F&N Orange</h3>
+        <p></p>
+        <div class="menu-item-price">RM3.00</div>
+      </div>
+      <div>
+        <button class="select-btn">Select</button>
+      </div>
+    </div>
+  </div>
+
+  <div class="menu-item" data-category="soft-drinks">
+    <img src="/TKCafe/public/images/TEA PING.png" alt="F&N Zapple" class="menu-item-img" />
+    <div class="menu-item-info">
+      <div>
+        <h3>F&N Zapple</h3>
+        <p></p>
+        <div class="menu-item-price">RM3.00</div>
+      </div>
+      <div>
+        <button class="select-btn">Select</button>
+      </div>
+    </div>
+  </div>
+
+  <div class="menu-item" data-category="soft-drinks">
+    <img src="/TKCafe/public/images/TEA PING.png" alt="F&N Strawberry" class="menu-item-img" />
+    <div class="menu-item-info">
+      <div>
+        <h3>F&N Strawberry</h3>
+        <p></p>
+        <div class="menu-item-price">RM3.00</div>
+      </div>
+      <div>
+        <button class="select-btn">Select</button>
+      </div>
+    </div>
+  </div>
+
+  <div class="menu-item" data-category="soft-drinks">
+    <img src="/TKCafe/public/images/F&N SODA.png" alt="F&N Soda" class="menu-item-img" />
+    <div class="menu-item-info">
+      <div>
+        <h3>F&N Soda</h3>
+        <p></p>
+        <div class="menu-item-price">RM3.00</div>
+      </div>
+      <div>
+        <button class="select-btn">Select</button>
+      </div>
+    </div>
+  </div>
+
+<!-- HOT DRINKS-->
+
+<div class="menu-item" data-category="hot-drinkss">
+    <img src="/TKCafe/public/images/MILO - 3 IN 1.png" alt="Cola Cola" class="menu-item-img" />
+    <div class="menu-item-info">
+      <div>
+        <h3>Milo - 3 IN 1 </h3>
+        <p></p>
+        <div class="menu-item-price">RM3.00</div>
+      </div>
+      <div>
+        <button class="select-btn">Select</button>
+      </div>
+    </div>
+  </div>
+
+  <div class="menu-item" data-category="hot-drinks">
+    <img src="/TKCafe/public/images/TEA.png" alt="Tea" class="menu-item-img" />
+    <div class="menu-item-info">
+      <div>
+        <h3> Tea </h3>
+        <p></p>
+        <div class="menu-item-price">RM3.00</div>
+      </div>
+      <div>
+        <button class="select-btn">Select</button>
+      </div>
+    </div>
+  </div>
+
+
+   <div class="menu-item" data-category="hot-drinks">
+    <img src="/TKCafe/public/images/TEA O.png" alt="Tea O" class="menu-item-img" />
+    <div class="menu-item-info">
+      <div>
+        <h3> Tea O </h3>
+        <p></p>
+        <div class="menu-item-price">RM2.00</div>
+      </div>
+      <div>
+        <button class="select-btn">Select</button>
+      </div>
+    </div>
+  </div>
+
+  <div class="menu-item" data-category="hot-drinks">
+    <img src="/TKCafe/public/images/COFFEE.png" alt="Coffee" class="menu-item-img" />
+    <div class="menu-item-info">
+      <div>
+        <h3>Coffee</h3>
+        <p></p>
+        <div class="menu-item-price">RM3.00</div>
+      </div>
+      <div>
+        <button class="select-btn">Select</button>
+      </div>
+    </div>
+  </div>
+
+   <div class="menu-item" data-category="hot-drinks">
+    <img src="/TKCafe/public/images/COFFEE O.png" alt="Coffee O" class="menu-item-img" />
+    <div class="menu-item-info">
+      <div>
+        <h3>Coffee O</h3>
+        <p></p>
+        <div class="menu-item-price">RM2.50</div>
+      </div>
+      <div>
+        <button class="select-btn">Select</button>
+      </div>
+    </div>
+  </div>
+
+  <div class="menu-item" data-category="hot-drinks">
+    <img src="/TKCafe/public/images/MINERAL WATER (500ML).png" alt="F&N Soda" class="menu-item-img" />
+    <div class="menu-item-info">
+      <div>
+        <h3>Mineral Water (500ML)</h3>
+        <p></p>
+        <div class="menu-item-price">RM1.50</div>
+      </div>
+      <div>
+        <button class="select-btn">Select</button>
+      </div>
+    </div>
+  </div>
+
+
+    
+  
 
 <?php require 'partials/footer.php'; ?>
 
--- Views/partials/categorybar.php
+++ Views/partials/categorybar.php
@@ -1,6 +1,7 @@
 <!-- Category bar -->
 <div class="category-bar">
   <button class="category active" data-category="all">All</button>
+  <button class="category" data-category="best-seller">Best Seller</button>
   <button class="category" data-category="standard">Ala Carte (Standard)</button>
   <button class="category" data-category="signature">Ala Carte (Signature)</button>
   <button class="category" data-category="set-standard">Set Standard Combo</button>
@@ -9,6 +10,7 @@
   <button class="category" data-category="masakan-side">Masakan Panas (Side Dish)</button>
   <button class="category" data-category="lokcing">Lokcing (Ala Carte)</button>
   <button class="category" data-category="western">Western Side Dish (Ala Carte)</button>
-  <button class="category" data-category="beverages">Beverages</button>
-  <button class="category" data-category="soft-drinks">Soft Drinks</button>
+  <button class="category" data-category="air-balang">Air Balang</button>
+   <button class="category" data-category="soft-drinks">Soft Drinks</button>
+  <button class="category" data-category="hot-drinks">Hot Drinks</button>
 </div>
