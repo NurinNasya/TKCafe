c690bb8b811c0d44abe05eb8f37718f965580121
hazsuh <nurhaziqahsuhaimi04@gmail.com>
2025-05-28 09:13:17 +0800

    updatecart

--- Views/partials/header.php
+++ Views/partials/header.php
@@ -4,10 +4,15 @@
     <meta charset="UTF-8" />
     <title>TK Cafe</title>
    <link rel="stylesheet" href="/TKCafe/public/css/style.css">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
 </head>
 <body>
 <header>
+     <div class="header-inner">
     <div class="logo">TK Cafe</div>
+     <a href="/TKCafe/cart.php" class="cart-icon">
+      <i class="fas fa-shopping-basket"></i>
+    </a>
     <nav>
     </nav>
 </header>
