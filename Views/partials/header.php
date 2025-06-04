0c526fa7e207d7a3ef0fa3732e5f56d67b7986f7
hazsuh <nurhaziqahsuhaimi04@gmail.com>
2025-06-04 11:27:31 +0800

    updt

--- Views/partials/header.php
+++ Views/partials/header.php
@@ -5,11 +5,19 @@
     <title>TK Cafe</title>
    <link rel="stylesheet" href="/TKCafe/public/css/style.css">
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
+<link href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@600&display=swap" rel="stylesheet">
+
+
+
 </head>
 <body>
 <header>
      <div class="header-inner">
-    <div class="logo">TK Cafe</div>
+     <div class="header-inner">
+    <div class="logo-title">
+     <img src="/TKCafe/public/images/tksb_logo.png" alt="Logo">
+      <h1>TK Cafe</h1>
+    </div>
      <a href="/TKCafe/Views/cart.php" class="cart-icon">
       <i class="fas fa-shopping-basket"></i>
     </a>
