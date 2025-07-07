61fd50e71347b486848d0a7caf9eb95f2c3b8bf9
NurinNasya <nurinnasya17@gmail.com>
2025-07-07 14:25:51 +0800

    lil

--- Views/menu_popup.php
+++ Views/menu_popup.php
@@ -1,36 +1,4 @@
 
-<!--<link rel="stylesheet" href="/TKCafe/public/css/style.css" />
-
-<div class="menu-details-container">
-  <h1 class="menu-title"><?= htmlspecialchars($item['name']) ?></h1>
-
-  <img src="<?= htmlspecialchars($item['image']) ?>" alt="<?= htmlspecialchars($item['name']) ?>" class="menu-image" />
-
-  <p class="menu-description"><?= htmlspecialchars($item['description']) ?></p>
-
-  <p class="menu-price">
-    <strong>Price:</strong> <?= htmlspecialchars($item['price']) ?>
-  </p>
-
-  <div class="menu-controls">
-    <div class="quantity-controls">
-      <button class="quantity-btn minus">−</button>
-      <input type="number" class="quantity-input" value="1" min="1" />
-      <button class="quantity-btn plus">+</button>
-    </div>
-
-    <button class="add-to-cart-btn" data-id="<?= htmlspecialchars($_GET['id']) ?>">
-      Add to Cart
-    </button>-->
-
-    <!--<div id="toast" class="toast">
-    <div class="toast-message">Added to Cart!</div>
-    </div>-->
-
-    <!--<a href="/TKCafe/Views/menu.php" class="back-button">⬅ Back to Menu</a>-->
-  <!--</div>
-</div>-->
-
 <?php if (!isset($item)) {
     echo "No item data.";
     exit;
@@ -113,9 +81,6 @@
         <input type="number" class="quantity-input" value="1" min="1" />
         <button class="quantity-btn plus" data-id="<?= htmlspecialchars($_GET['id']) ?>">+</button>
 
-      <!-- <button class="quantity-btn minus">−</button>
-      <input type="number" class="quantity-input" value="1" min="1" />
-      <button class="quantity-btn plus">+</button> -->
     </div>
 
     <button class="add-to-cart-btn" data-id="<?= htmlspecialchars($_GET['id']) ?>">
